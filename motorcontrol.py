69bdd6eb50698f4b45b19eaa51b52508167154a1
kornh <kh@h-four.de>
2017-05-23 13:50:33 +0200

    Changed Board to GPIO

---
+++ motorcontrol.py
@@ -0,0 +1,43 @@
+""" motorcontrol """
+#import time                     # Zeitsteuerung
+from RPi import GPIO
+#import RPi.GPIO as GPIO         # Ansteuerung der GPIO
+
+print("Steuerprogram gestartet")
+print("Bibliotheken geladen")
+
+## Deklaration der Variablen
+#pin_motor_1_1 = 27
+#pin_motor_1_2 = 22
+#pin_motor_2_1 = 23
+#pin_motor_2_2 = 24
+PINS = [
+    [27, 22],
+    [23, 24]
+]
+
+print("Variablen deklariert")
+
+## Setzten der Portrichtung
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PINS, GPIO.OUT)
+#GPIO.setup(PINS[0][0], GPIO.OUT)
+#GPIO.setup(PINS[0][1], GPIO.OUT)
+#GPIO.setup(PINS[1][0], GPIO.OUT)
+#GPIO.setup(PINS[1][1], GPIO.OUT)
+
+print("Portrichtungen gesetzt")
+
+def set_pin(motor, status):
+    """Set the pins to control the motors"""
+    GPIO.output(PINS, 0)
+
+    motor = (0 if motor == 1 else 1)
+    status = (0 if status > 0 else 1)
+    pin = PINS[motor][status]
+    GPIO.output(pin, 1)
+
+'''
+    time.sleep(1)
+    GPIO.cleanup()          # Bei Programabbruch GPIO zuruecksetzten
+'''
--- webcontrol.py
+++ webcontrol.py
@@ -1,131 +1,82 @@
-"""
-simplewebcontrol.py
-Controls PiFace Digital through a web browser. Returns the status of the
-input port and the output port in a JSON string. Set the output with GET
-variables.
-
-Copyright (C) 2013 Thomas Preston <thomas.preston@openlx.org.uk>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+#!/usr/bin/python
+""" webcontrol """
 import sys
-import subprocess
-import urllib.parse
-import http.server
-import pifacedigitalio
+from urllib import parse
+from http import server
+from motorcontrol import set_pin
 
-
-JSON_FORMAT = "{{'relay': {num}, 'status': {status}}}"
+JSON_FORMAT = "{{'motor': {num}, 'status': {status}}}"
 DEFAULT_PORT = 8000
-OUTPUT_PORT_GET_STRING = "relay"
 GET_IP_CMD = "hostname -I"
 
 
-class PiFaceWebHandler(http.server.BaseHTTPRequestHandler):
+class PiFaceWebHandler(server.BaseHTTPRequestHandler):
     """Handles PiFace web control requests"""
+    def parse_path(self, path):
+        """Parse URL-Path"""
+        return parse.parse_qs(parse.urlparse(path).query)
+
     def do_GET(self):
         """GET Handler"""
         # parse the query string
-        qss = urllib.parse.urlparse(self.path).query
-        query_components = urllib.parse.parse_qs(qss)
+        query_components = self.parse_path(self.path)
+        print(query_components)
 
-        relay = self.get_relay_value(query_components)
-        status = self.get_status_value(query_components)
-        print("Request Relay:{} Status:{}.".format(relay, status))
-        #'No valid status!'
+        message = ""
+        try:
+            motor = self.get_value(query_components, 'motor')
+        except ValueError as err:
+            message += 'Motor: '+str(err)+'\n'
+        try:
+            status = self.get_value(query_components, 'status')
+        except ValueError as err:
+            message += 'Status: '+str(err)+'\n'
 
-        if relay >= 0 and status >= 0:
-            self.set_relay(relay, status)
-            self.response(relay, status)
+        if message == '' and (motor <= 0 or motor >= 3):
+            message += 'Motor: Value not in range!'
+
+        if message != '':
+            self.error_response(message)
             return
-        if int(relay) < 0:
-            if relay == -1:
-                relay = 'No relay given!'
-            if relay == -2:
-                relay = 'No valid relay!'
-            status = 'Status unused!'
-            self.error_response(relay, status)
-            return
-        if int(status) < 0:
-            if status == -1:
-                status = self.get_relay(relay)
-            if status == -2:
-                status = 'No valid status!'
-            self.error_response(relay, status)
 
-    def response(self, relay, status):
+        print("Request Motor:{} Status:{}.".format(motor, status))
+
+        set_pin(motor, status)
+        self.success_response(motor, status)
+
+    def success_response(self, motor, status):
+        """Response with SUCCESS"""
+        message = JSON_FORMAT.format(num=motor, status=status)
+        self.response(200, message)
+
+    def error_response(self, message):
+        """Response with ERROR"""
+        self.response(404, message)
+
+    def response(self, code, message):
         """Response to client"""
         # reply with JSON
-        self.send_response(200)
+        self.send_response(code)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        self.wfile.write(bytes(JSON_FORMAT.format(
-            num=relay,
-            status=status,
-        ), 'UTF-8'))
+        self.wfile.write(bytes(message, 'UTF-8'))
 
-    def error_response(self, relay, status):
-        """Response to client"""
-        # reply with JSON
-        self.send_response(404)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(JSON_FORMAT.format(
-            num=relay,
-            status=status,
-        ), 'UTF-8'))
-
-    def get_relay_value(self, query_components):
-        """Gets the relay query value."""
-        if 'relay' not in query_components:
-            return -1
-        relay = query_components["relay"][0]
+    def get_value(self, query, name):
+        """Gets the query value."""
+        if name not in query:
+            raise ValueError('Value not given!')
+        value = query[name][0]
         try:
-            relay_num = int(relay)
-            if relay_num > 1 or relay_num < 0:
-                raise ValueError('relay out of range!')
+            num = int(value)
         except ValueError:
-            return -2
-        return relay_num
-
-    def get_status_value(self, query_components):
-        """Gets the status query value."""
-        if 'status' not in query_components:
-            return -1
-        status = query_components["status"][0]
-        try:
-            port_value = int(status)  # dec
-            if port_value > 1 or port_value < 0:
-                raise ValueError('status out of range!')
-        except ValueError:
-            return -2
-        return port_value
-
-    def set_relay(self, relay, port_value):
-        """Sets the relay status value."""
-        print("Setting relay {} to {}.".format(relay, port_value))
-        self.pifacedigital.relays[relay].value = port_value
-
-    def get_relay(self, relay):
-        """Gets the relay status value."""
-        return self.pifacedigital.relays[relay].value
+            raise ValueError('Given Value is not valid!')
+        return num
 
 def get_my_ip():
     """Returns this computers IP address as a string."""
-    ips = subprocess.check_output(GET_IP_CMD, shell=True).decode('utf-8')[:-1]
-    return ips.strip()
-
+    #ips = subprocess.check_output(GET_IP_CMD, shell=True).decode('utf-8')[:-1]
+    #return ips.strip()
+    return 'localhost'
 
 if __name__ == "__main__":
     # get the port
@@ -135,7 +86,6 @@ if __name__ == "__main__":
         PORT = DEFAULT_PORT
 
     # set up PiFace Digital
-    PiFaceWebHandler.pifacedigital = pifacedigitalio.PiFaceDigital()
 
     print("Starting simple PiFace web control at:\n\n"
           "\thttp://{addr}:{port}\n\n"
@@ -144,7 +94,7 @@ if __name__ == "__main__":
     # run the server
     SERVER_ADDRESS = ('', PORT)
     try:
-        HTTPD = http.server.HTTPServer(SERVER_ADDRESS, PiFaceWebHandler)
+        HTTPD = server.HTTPServer(SERVER_ADDRESS, PiFaceWebHandler)
         HTTPD.serve_forever()
     except KeyboardInterrupt:
         print('^C received, shutting down server')
